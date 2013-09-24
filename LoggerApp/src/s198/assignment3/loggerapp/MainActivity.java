d29124233d6f72ffa901df7281f293f30351f740
Keertan Kini <krkini16@mit.edu>
2013-09-24 02:05:20 -0400

    Added logging capabilities, as well as created the original Android project

---
+++ LoggerApp/src/s198/assignment3/loggerapp/MainActivity.java
@@ -0,0 +1,31 @@
+package s198.assignment3.loggerapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+	private static final String TAG = "LoggerActivity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Log.d(TAG, "THIS IS MY LOGGING LINE --- IT'S GREAT FOR DEBUGGING");
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.d(TAG, "I got onPause!!!");
+	}
+}
